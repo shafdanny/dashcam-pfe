404790b1dc9a0ed3cd8930c5f21aefc66bdd53db
Paltoquet <thibault.ober@gmail.com>
2017-02-23 16:49:41 +0100

    Trivium on GPU and cpu

---
+++ cpu_programming/cpu_test/crypt.cpp
@@ -0,0 +1,49 @@
+#include "trivium.h"
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+//g++ -o crypt crypt.cpp trivium.cpp
+
+
+
+int main(){
+    
+    FILE *fp;
+    fp = fopen("images/blossom.jpg","rb");  // r for read, b for binary
+    fseek(fp, 0, SEEK_END);
+    int filelen = ftell(fp);
+    rewind(fp);
+    unsigned char* buffer = (unsigned char *)malloc((filelen+1)*sizeof(char));
+    fread(buffer,filelen,1,fp);
+    fclose(fp);
+    
+    Trivium trivium;
+    trivium.initiation();
+    unsigned char* result = trivium.cypher(buffer,filelen);
+    
+    cout << "end encryption" << endl;
+    fp = fopen("image_crypted_test1.jpg", "wb");
+    fwrite(result, sizeof(result[0]), filelen/sizeof(result[0]), fp);
+    fclose(fp);
+    
+    
+    fp = fopen("images/cat.jpg","rb");  // r for read, b for binary
+    fseek(fp, 0, SEEK_END);
+    filelen = ftell(fp);
+    rewind(fp);
+    buffer = (unsigned char *)malloc((filelen+1)*sizeof(char));
+    fread(buffer,filelen,1,fp);
+    fclose(fp);
+
+    result = trivium.cypher(buffer,filelen);
+    
+    fp = fopen("image_crypted_test2.jpg", "wb");
+    fwrite(result, sizeof(result[0]), filelen/sizeof(result[0]), fp);
+    fclose(fp);
+    
+}
